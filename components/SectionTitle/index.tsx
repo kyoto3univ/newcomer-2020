d68d482d22afcc4a780362489c042ff18f041c74
f0reachARR <f0reach@f0reach.me>
2020-04-02 21:28:45 +0900

    Add initial top page

---
+++ components/SectionTitle/index.tsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const SectionTitle = styled.h2`
+  border-bottom: 3px solid #f70;
+`;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,21 @@
+import Head from 'next/head';
 import { Container } from '../components/Container';
+import { SectionTitle } from '../components/SectionTitle';
 
 export default () => {
   return (
-    <Container>
-      <p>
-        このサイトでは、COVID-19の影響で新歓や初回授業を受けられなくなった新入生をサポートする情報を配信しています。
-      </p>
-    </Container>
+    <>
+      <Head>
+        <title>京都三大学新入生応援サイト</title>
+      </Head>
+      <Container>
+        <section>
+          <SectionTitle>このサイトについて</SectionTitle>
+          <p>
+            このサイトでは、COVID-19の影響で新歓や初回授業を受けられなくなった新入生をサポートする情報を配信しています。
+          </p>
+        </section>
+      </Container>
+    </>
   );
 };
