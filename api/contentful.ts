e9db9229ddfbce6bf109816aa87ed003118335fe
f0reachARR <f0reach@f0reach.me>
2020-04-03 12:35:29 +0900

    Support for api host update

--- api/contentful.ts
+++ api/contentful.ts
@@ -3,6 +3,7 @@ import { Asset, createClient, Entry } from 'contentful';
 const client = createClient({
   space: process.env.SPACE_ID as string,
   accessToken: process.env.ACCESS_TOKEN as string,
+  host: process.env.CONTENTFUL_HOST || 'cdn.contentful.com',
 });
 
 type Category = {
