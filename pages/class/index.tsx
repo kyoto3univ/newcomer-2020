d08b5f1804baf4f4c20404c7b92df9ba24b15841
f0reachARR <f0reach@f0reach.me>
2020-04-13 19:38:38 +0900

    Add tag filter for classes

--- pages/class/index.tsx
+++ pages/class/index.tsx
@@ -15,10 +15,21 @@ const FilterContainer = styled.div`
   display: flex;
   align-items: center;
   margin-bottom: 0.8em;
+  flex-wrap: wrap;
 `;
 
 const SelectContainer = styled.div`
   width: 100%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+
+  .hour {
+    min-width: 8em;
+  }
+  .tags {
+    flex-grow: 1;
+  }
 `;
 
 const ClearLink = styled.a`
@@ -29,57 +40,106 @@ const ClearLink = styled.a`
 type Props = {
   classes: ExtractPromise<ReturnType<typeof fetchClassList>>;
   hours: Array<{ day: string; time: number }>;
+  tags: string[];
 };
-type OptionType = {
+type TimeOptionType = {
   label: string;
   value: { day: string; time: number };
 };
-export default ({ classes, hours }: Props) => {
+type TagsOptionType = {
+  label: string;
+  value: string;
+};
+export default ({ classes, hours, tags }: Props) => {
   const router = useRouter();
   const [hourFilter, setHourFilter] = React.useState<{
     day: string;
     time: number;
   } | null>(null);
+  const [tagFilter, setTagFilter] = React.useState<string[]>([]);
 
   React.useEffect(() => {
-    if (decodeURIComponent(router.asPath).match(/#filter=(\w+)-(\d+)/g)) {
+    const asPath = decodeURIComponent(router.asPath);
+    if (asPath.match(/[#&]filter=(\w+)-(\d+)/g)) {
       setHourFilter({
         day: RegExp.$1,
         time: Number(RegExp.$2),
       });
     }
+    if (asPath.match(/[#&]tags=(.+)/g)) {
+      const filter = RegExp.$1?.split(',') || [];
+      setTagFilter(filter.filter((item) => tags.includes(item)));
+    }
   }, [router, router.asPath]);
 
   const filteredClasses = React.useMemo(() => {
-    if (!hourFilter) return classes;
+    if (!hourFilter && tagFilter.length === 0) return classes;
 
     return classes.filter(
-      ({ day, time }) => day === hourFilter.day && time === hourFilter.time,
+      ({ day, time, tags }) =>
+        (!hourFilter || (day === hourFilter.day && time === hourFilter.time)) &&
+        (tagFilter.length === 0 || tagFilter.some((tag) => tags.includes(tag))),
     );
-  }, [hourFilter]);
+  }, [hourFilter, tagFilter]);
 
-  const selectOptions: OptionsType<OptionType> = hours.map((hour) => ({
+  const hourSelectOptions: OptionsType<TimeOptionType> = hours.map((hour) => ({
     value: hour,
     label: `${hour.day}曜${hour.time}限`,
   }));
 
-  const selectedOptions: OptionType | null = hourFilter
+  const tagSelectOptions: OptionsType<TagsOptionType> = tags.map((tag) => ({
+    value: tag,
+    label: tag,
+  }));
+
+  const hourSelectedOptions: TimeOptionType | null = hourFilter
     ? {
         value: hourFilter,
         label: `${hourFilter.day}曜${hourFilter.time}限`,
       }
     : null;
 
-  const handleSelectChange = React.useCallback(
-    (options: ValueType<OptionType>) => {
+  const tagSelectedOptions: OptionsType<TagsOptionType> = tagFilter.map(
+    (tag) => ({
+      value: tag,
+      label: tag,
+    }),
+  );
+
+  const handleHourSelectChange = React.useCallback(
+    (options: ValueType<TimeOptionType>) => {
       // if (options) setCategoryFilter([(options as OptionType).value]);
       if (options) {
-        const filter = (options as OptionType).value;
+        const filter = (options as TimeOptionType).value;
 
         setHourFilter(filter);
         router.replace({
           pathname: router.pathname,
-          hash: `filter=${filter.day}-${filter.time}`,
+          hash: `filter=${filter.day}-${filter.time}&tags=${tagFilter.join(
+            ',',
+          )}`,
+        });
+      }
+    },
+    [router.pathname],
+  );
+
+  const handleTagSelectChange = React.useCallback(
+    (options: ValueType<TagsOptionType>) => {
+      // if (options) setCategoryFilter([(options as OptionType).value]);
+      if (options) {
+        const filter = (options as OptionsType<TagsOptionType>).map(
+          ({ value }) => value,
+        );
+
+        setTagFilter(filter);
+        router.replace({
+          pathname: router.pathname,
+          hash: hourFilter
+            ? `filter=${hourFilter.day}-${hourFilter.time}&tags=${filter.join(
+                ',',
+              )}`
+            : `tags=${filter.join(',')}`,
         });
       }
     },
@@ -89,6 +149,7 @@ export default ({ classes, hours }: Props) => {
   const handleClearFilter = React.useCallback(
     (e: React.SyntheticEvent) => {
       setHourFilter(null);
+      setTagFilter([]);
       router.replace({
         pathname: router.pathname,
         hash: null,
@@ -119,10 +180,7 @@ export default ({ classes, hours }: Props) => {
             その場合は、その時間の科目を履修しないか、受講登録修正期間に空き定員のある授業に登録することになります。
           </p>
           <p>
-            ・卒業認定に必要な単位数は、科目分類ごとに指定されているので、履修計画を立てて、授業を選択してください。(履修要項2020を確認してください)
-          </p>
-          <p>
-            {`・共同化科目の科目分類は"人と自然", "人と社会", "人と文化","体の科学", "京の伝統文化と先端科学技術", "環境・倫理"の6種類です。`}
+            ・卒業認定に必要な単位数が、科目分類ごとに指定されている大学もあります。(科目分類：人と自然、人と文化、人と社会etc…)
           </p>
         </section>
         <section>
@@ -130,13 +188,22 @@ export default ({ classes, hours }: Props) => {
           <FilterContainer>
             <SelectContainer>
               <Select
-                options={selectOptions}
-                onChange={handleSelectChange}
-                value={selectedOptions}
-                placeholder='時間で絞り込み'
+                options={hourSelectOptions}
+                onChange={handleHourSelectChange}
+                value={hourSelectedOptions}
+                placeholder='時限'
+                className='hour'
+              />
+              <Select
+                options={tagSelectOptions}
+                onChange={handleTagSelectChange}
+                value={tagSelectedOptions}
+                placeholder='タグで絞り込み'
+                isMulti
+                className='tags'
               />
             </SelectContainer>
-            {hourFilter && (
+            {(hourFilter || tagFilter.length > 0) && (
               <ClearLink onClick={handleClearFilter} href='#'>
                 絞り込み解除
               </ClearLink>
@@ -161,6 +228,19 @@ export const getStaticProps: GetStaticProps<Props> = async () => {
             ...obj,
             [`${day}-${time}`]: { day, time },
           }),
+          {},
+        ),
+      ),
+      tags: Object.keys(
+        classes.reduce<Record<string, number>>(
+          (obj, { tags }) =>
+            tags.reduce(
+              (obj2, tag) => ({
+                ...obj2,
+                [tag]: 1,
+              }),
+              obj,
+            ),
           {},
         ),
       ),
